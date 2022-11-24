01afd38ca91b733aad0779908160ed73010aefbb
lesniak6u <louis.lesniak4@etu.univ-lorraine.fr>
2022-11-24 09:01:29 +0100

    Affichage d'une recette

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>
+    <div class="boiteRecette">
+        <img src="Photos/Mojito.jpg" alt="Mojito" style="width:100%">
+        <div class="boiteTitreRecette">
+            <p class="titreRecette">Mojito</p>
+        </div>
+    </div>
+
+    </body>
+</html>
+
