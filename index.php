7ac677f835fc94451579ca721881d022d83f0b90
joffroy8u <theo.joffroy7@etu.univ-lorraine.fr>
2022-11-24 10:02:58 +0100

    Organisation page principale

--- index.php
+++ index.php
@@ -4,11 +4,49 @@
     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 <body>
+    <div class="header"> 
+        <h1> Easy Drinks </h1>
+    </div>
+
+    <ul class="navigation">
+        <li><a href="#">Catégorie 1</a></li>
+        <li><a href="#">Catégorie 2</a></li>
+        <li>Catégorie 3</li>
+    </ul>
+
+    <form class="rechercheTexte">
+        <input type="text" id="champRecherche">
+        <button id="boutonRechercheTexte"> Rechercher </button>
+    </form>
+
+    <div class="listeCategories">
+        <div class="categorie">
+            <a href="#">Catégorie 1</a>
+        </div>
+        
+        <div class="categorie">
+            <a href="#">Catégorie 2</a>
+        </div>
+
+        <div class="categorie">
+            <a href="#">Catégorie 3</a>
+        </div>
+    </div>
+
     <div class="boiteRecette">
         <img src="Photos/Mojito.jpg" alt="Mojito" style="width:100%">
         <div class="boiteTitreRecette">
             <p class="titreRecette">Mojito</p>
         </div>
+    </div>
+
+    <div class="listeIngredients">
+        <p>Ingrédient 1</p>
+        <p>Ingrédient 2</p>
+        <p>Ingrédient 3</p>
+        <form class="rechercheRecettes">
+            <button id="boutonRechercheRecette"> Rechercher des recettes </button>
+        </form>
     </div>
 
     </body>
