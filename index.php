5620fad1f32396405886e29f292e36b17ad72dac
lesniak6u <louis.lesniak4@etu.univ-lorraine.fr>
2022-12-19 16:55:24 +0100

    Recettes par catégorie

--- index.php
+++ index.php
@@ -21,6 +21,16 @@
                         $('.listeCategories').append("<div class='categorie'> <a class='boutonMenu' href='#'>" + value + "</a></div>");
                     });
                 })
+
+
+                $.post("recettes.php", 
+                {
+                    current: $(this).text()
+                }, function(data, status){
+                    alert(data);
+                    $('.containerRecettes').empty();
+                    $('.containerRecettes').append(data);
+                })
             });
         });
     </script>
@@ -55,28 +65,7 @@
                 <li>Catégorie 3</li>
             </ul>
             <div class="containerRecettes">
-                <?php
-                    require 'Donnees.inc.php';
-
-                    $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-                                                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-                                                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'ç'=>'c',
-                                                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                                                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', ' ' => '_', '\'' => '', '"' => '' );
-
-                    foreach($Recettes as $recette)
-                    {
-                        $sansAccents = strtr($recette['titre'], $unwanted_array );
-                        $nomBoisson = ucwords(strtolower($sansAccents));
-
-                        echo '<div class="boiteRecette">
-                                <img src="Photos/'.(file_exists('Photos/'.$nomBoisson.'.jpg')?$nomBoisson:'Notfound').'.jpg" alt="'.$nomBoisson.'" class="imageRecette">
-                                <div class="boiteTitreRecette">
-                                    <p class="titreRecette">'.$recette['titre'].'</p>
-                                </div>
-                            </div>';
-                    }
-                ?>
+                
             </div>
         </div>
 
