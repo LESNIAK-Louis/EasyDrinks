48ee7a56c01ab744cfdc0eeee2b084bd0569309b
lesniak6u <louis.lesniak4@etu.univ-lorraine.fr>
2022-12-21 17:12:06 +0100

    Ajout des etoiles

---
+++ panier.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+    <title>EasyDrinks - Compte</title>
+    <meta charset="utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+ </head>
+ <body>
+    <div class="header"> 
+        <a href="./index.php?#" class="titre"> <h1> Easy Drinks </h1> </a>
+        
+    </div>
+
+
+    <div class="menu">
+
+    </div>
+
+
+    <div class="main">
+            <div class="containerRecettes"></div>
+            <div class="contenuRecette"></div>
+    </div>
+
+    <div class="right">
+
+    </div>
+
+    <div class="footer">
+    </div>
+</body>
+</html>
